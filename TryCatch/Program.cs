3a4f8a822074873974bd52e57f7c89ad6f6e1275
Wendry bergamasco <2022102020033@aluno.ifro.edu.br>
2023-06-06 14:03:16 -0400

    Primeiro Commit

---
+++ TryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿
+try
+{
+    Console.Write("Informe o primeiro valor!");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Informe o segundo valor!");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+
+}catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! + {ex.Message}");
+}catch(InvalidCastException ex)
+{
+    Console.WriteLine($"Conversão de string para letra não é permitida! {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Aqui passa de qualquer forma!");
+}
